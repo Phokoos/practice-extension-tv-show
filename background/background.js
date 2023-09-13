0f10a57f04da00f9a1991323923d4b774c809387
Микола <hulkovskij@gmail.com>
2023-09-13 22:57:21 +0300

    Finished 33

--- background/background.js
+++ background/background.js
@@ -1,22 +1,30 @@
+chrome.storage.local.set({
+	shows: []
+})
+
 chrome.runtime.onInstalled.addListener((details) => {
 	chrome.contextMenus.create({
 		title: "Test Context Menu",
 		id: "contextMenu1",
 		contexts: ["page", 'selection']
 	})
+	chrome.contextMenus.create({
+		title: "Read This Text",
+		id: "contextMenu2",
+		contexts: ["page", 'selection']
+	})
 	chrome.contextMenus.onClicked.addListener((event) => {
-		console.log(event);
-		chrome.tabs.create({
-			// active: true,
-			url: `https://www.imdb.com/find/?q=${event.selectionText}&ref_=nv_sr_sm`
-		})
+		if (event.menuItemId === "contextMenu1") {
+			fetch(`https://api.tvmaze.com/search/shows?q=${event.selectionText}`)
+				.then((res) => res.json())
+				.then(data => {
+					console.log(data)
+					chrome.storage.local.set({
+						shows: data
+					})
+				})
+		} else if (event.menuItemId === "contextMenu2") {
+			chrome.tts.speak(event.selectionText)
+		}
 	})
 })
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	console.log(message);
-	console.log(sender);
-	// sendResponse("received message on back")
-	chrome.tabs.sendMessage(sender.tab.id, "Got your message, from back")
-})
-
--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,21 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-	console.log(msg);
-	console.log(sender);
+
+chrome.storage.local.get(["shows"], (res) => {
+	for (const show of res.shows) {
+		renderShow(show)
+	}
 })
+
+function renderShow(show) {
+	const showDiv = document.createElement("div")
+
+	const title = document.createElement("h3")
+	title.textContent = show.show.name
+	console.log(title);
+
+	const image = document.createElement("img")
+	image.src = show.show.image ? show.show.image.medium : null
+
+	showDiv.appendChild(title)
+	showDiv.appendChild(image)
+	document.body.appendChild(showDiv)
+}
